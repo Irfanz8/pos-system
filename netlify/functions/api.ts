93c25ecdce40dac54f24d8535208383699817e1b
Irfan Aprilyana <52303297+Irfanz8@users.noreply.github.com>
2026-03-08 14:50:06 +0700

    fix: Correct API entry point import extension from .js to .ts.

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,4 @@
 import serverless from 'serverless-http';
-import app from '../../packages/api/src/index.js';
+import app from '../../packages/api/src/index.ts';
 
 export const handler = serverless(app);
